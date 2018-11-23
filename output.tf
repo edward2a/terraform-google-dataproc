43266d30a82872662278988b31af256304817570
Eduardo A. Paris Penas <edward2a@gmail.com>
2018-11-23 11:29:19 +0000

    working outputs

--- output.tf
+++ output.tf
@@ -1,4 +1,15 @@
 output "dataproc_master_instance_names" {
     description = "List of master instance names which have been assigned to the cluster"
-    value = "${google_dataproc_cluster.this.cluster_config.master_config.instance_names}"
+    value = "${join(",", google_dataproc_cluster.this.cluster_config.0.master_config.0.instance_names)}"
 }
+
+output "dataproc_worker_instance_names" {
+  description = "List of worker instance names which have been assigned to the cluster"
+  value = "${var.worker_num_instances > 0 ? join(",", google_dataproc_cluster.this.cluster_config.0.worker_config.0.instance_names) : ""}"
+}
+
+output "dataproc_preemptible_worker_instance_names" {
+  description = "List of preemptible woreker instance names which ave been assigned to the cluster"
+  value = "${var.preemptible_num_instances > 0 ? join(",", google_dataproc_cluster.this.cluster_config.0.preemptible_worker_config.0.instance_names) : ""}"
+}
+
